559dac508e0c605fb7a2d59475ae539e7d66fdc9
xovest <79008917+xovest@users.noreply.github.com>
2022-01-17 21:33:41 +0200

    basic boilerplate

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Button, Stack } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+
+function App() {
+  return <Container className='my-4'>
+    <Stack direction='horizontal' gap='2' className='mb-4'>
+      <h1 className='me-auto'>Budgetz</h1>
+      <Button variant='primary'>Add'em budge</Button>
+      <Button variant='outline-primary'>Add'em expens</Button>
+    </Stack>
+    <div 
+      style={{ 
+        display: 'grid', 
+        gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', 
+        gap: '1rem', 
+        alignItems: 'flex-start',
+      }}>
+    </div>
+  </Container>
+}
+
+export default App
